7d2a4eb025f4441ef76b5f585459cd3904330acf
levchenko.victor@gmail.com <levchenko.victor@gmail.com>
2023-09-22 16:05:01 +0300

    Leetcode 1 : Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.

---
+++ Leetcode/1/solution.cpp
@@ -0,0 +1,52 @@
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+Example 1:
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+Example 2:
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+
+Example 3:
+Input: nums = [3,3], target = 6
+Output: [0,1]
+*/
+
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>&& nums, int target) {
+        
+        unordered_map<int, int> numMap;
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++) {
+            int complement = target - nums[i];
+            if (numMap.count(complement)) {
+                return {numMap[complement], i};
+            }
+            numMap[nums[i]] = i;
+        }
+
+        return {}; // No solution found
+    }
+};
+
+int main() {
+
+    Solution s;
+    auto res = s.twoSum({3,2,4}, 6);
+
+    return 0;
+}
